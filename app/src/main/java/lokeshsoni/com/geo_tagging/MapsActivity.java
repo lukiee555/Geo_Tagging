d4d438472ce4e3fc7188c44098c0bff684940ab6
lukiee555 <sonilokesh24@gmail.com>
2017-09-01 17:04:03 +0530

    Test Working

--- app/src/main/java/lokeshsoni/com/geo_tagging/MapsActivity.java
+++ app/src/main/java/lokeshsoni/com/geo_tagging/MapsActivity.java
@@ -1,5 +1,8 @@
 package lokeshsoni.com.geo_tagging;
 
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.drawable.BitmapDrawable;
 import android.location.Location;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
@@ -8,15 +11,18 @@ import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
 import java.io.InputStream;
+import java.util.ArrayList;
 import java.util.List;
 
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
 
     private GoogleMap mMap;
+    String[] images = {"chrome.png","facebook.png","p.pmg","youtube.png"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -38,17 +44,25 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         InputStream inputStream = getResources().openRawResource(R.raw.nw);
         CSVFile csvFile = new CSVFile(inputStream);
         List scoreList = csvFile.read();
-        String[] score = (String[]) scoreList.get(0);
-//        Location location = new Location("");
-//
-//        location.setLatitude(Double.parseDouble(score[7]));
-//        location.setLongitude(Double.parseDouble(score[8]));
 
 
-        LatLng imageLocation = new LatLng(Double.parseDouble(score[7]), Double.parseDouble(score[8]));
+        String[] score ;
 
-        mMap.addMarker(new MarkerOptions().position(imageLocation).title("Marker in ImageLocation"));
-        mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(imageLocation, 12.0f));
+        LatLng imageLocation;
+        int height = 700;
+        int width = 700;
+        BitmapDrawable bitmapdraw=(BitmapDrawable)getResources().getDrawable(R.drawable.img);
+        Bitmap b=bitmapdraw.getBitmap();
+        Bitmap smallMarker = Bitmap.createScaledBitmap(b, width, height, false);
+
+
+         for(int i=7;i<15;i++) {
+             score= (String[]) scoreList.get(i);
+             imageLocation = new LatLng(Double.parseDouble(score[7]), Double.parseDouble(score[8]));
+             mMap.addMarker(new MarkerOptions().position(imageLocation).title("Marker in ImageLocation").icon(BitmapDescriptorFactory.fromBitmap(smallMarker)));
+             mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(imageLocation, 21));
+
+         }
 
     }
 }
